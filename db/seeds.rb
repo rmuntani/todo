139bb4947098b6de04456b0627df235878dfc1ad
Raphael Montani <raphael.muntani@gmail.com>
2025-12-15 19:02:17 -0300

    Add seeds to simplify testing manually the new scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+providers = 20.times.map do
+  Provider.create(email: Faker::Internet.email,
+                  name: Faker::Name.name)
+end
+
+clients = 20.times.map do
+  Client.create(email: Faker::Internet.email,
+                name: Faker::Name.name)
+end
+
+plans = clients.flat_map.with_index do |client, idx|
+  3.times.map do |delta|
+    Plan.create(client: client,
+      tier: ['premium', 'basic'].sample,
+      provider: providers[(idx + delta) % providers.length]
+    )
+  end
+end
+
+clients.each do |client|
+  3.times do
+    JournalEntry.create(client: client, entry: Faker::Lorem.paragraph)
+  end
+end
