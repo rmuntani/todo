270e0498eeaf1fb071eb39b4ee5b80e519a2ccce
Raphael Montani <raphael.muntani@gmail.com>
2025-12-16 16:17:27 -0300

    Format JS and TSX code with biome
    The default configuration is used, as we only want to ensure the project
    follows best-practices in formatting and linting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,130 +1,146 @@
-import './App.css';
-import { Column } from './components/Column';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import { Column } from "./components/Column";
+import { useState, useEffect } from "react";
 
 // I had to Google for this lib https://www.npmjs.com/package/react-confetti
-import Confetti from 'react-confetti'
+import Confetti from "react-confetti";
 
 // I had to Google how to implement drag and drop and found out https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API,
 // which I used as a reference.
 
 interface Tasks {
-	todos: Array<Task>,
-	doing: Array<Task>,
-	done: Array<Task>
+	todos: Array<Task>;
+	doing: Array<Task>;
+	done: Array<Task>;
 }
 
 interface Task {
-	id: number,
-	task: string
+	id: number;
+	task: string;
 }
 
-interface Column { column: 'todos' | 'doing' | 'done' | null }
+interface ColumnTypes {
+	column: "todos" | "doing" | "done" | null;
+}
 
-type DraggedItem = Task & Column
+type DraggedItem = Task & ColumnTypes;
 
 function App() {
-  const [tasks, updateTasks] = useState<Tasks>({ todos: [], doing: [], done: []})
-  const [dragged, dragItem] = useState<DraggedItem>({ task: '', column: null })
-  const [confettiEnabled, enableConfetti] = useState<bool>(false)
+	const [tasks, updateTasks] = useState<Tasks>({
+		todos: [],
+		doing: [],
+		done: [],
+	});
+	const [dragged, dragItem] = useState<DraggedItem>({ task: "", column: null });
+	const [confettiEnabled, enableConfetti] = useState<bool>(false);
 
-  useEffect(() => {
+	useEffect(() => {
 		const timer = setTimeout(() => enableConfetti(false), 5000);
 
 		return () => clearTimeout(timer);
-  }, [confettiEnabled]);
+	}, [confettiEnabled]);
 
-  const totalItems = () => tasks.todos.length + tasks.doing.length + tasks.done.length
-  const addTodo = (event) => {
-		const newTodo : string|null = event.target.todo.value
-		event.preventDefault()
-		
-		if(newTodo !== '') {
-			const taskId = totalItems() + 1
+	const totalItems = () =>
+		tasks.todos.length + tasks.doing.length + tasks.done.length;
+	const addTodo = (event) => {
+		const newTodo: string | null = event.target.todo.value;
+		event.preventDefault();
+
+		if (newTodo !== "") {
+			const taskId = totalItems() + 1;
 			updateTasks({
 				...tasks,
-				todos: [...tasks.todos, { task: newTodo, id: taskId }]
-			})
+				todos: [...tasks.todos, { task: newTodo, id: taskId }],
+			});
 
-			event.target.todo.value = ''
+			event.target.todo.value = "";
 		}
-  }
+	};
 
-  const startDrag = (item : DraggedItem) => {
-		dragItem(item)
-  }
+	const startDrag = (item: DraggedItem) => {
+		dragItem(item);
+	};
 
-  const receiveItem = (event, newColumn : string) => {
-		const { task, column, id } = dragged
+	const receiveItem = (newColumn: string) => {
+		const { task, column, id } = dragged;
 
-		if (newColumn == column) return;
+		if (newColumn === column) return;
 
-		const removeFrom = tasks[column]
+		const removeFrom = tasks[column];
 		const addTo = tasks[newColumn];
-		const newState = { 
+		const newState = {
 			...tasks,
 			[column]: removeFrom.filter(({ id: taskId }) => taskId !== id),
-			[newColumn]: [...addTo, { task, id }]
+			[newColumn]: [...addTo, { task, id }],
+		};
+
+		updateTasks(newState);
+		if (newColumn === "done") {
+			enableConfetti(true);
 		}
+		dragItem({ task: "", column: null, id: 0 });
+	};
 
-		updateTasks(newState)
-		if (newColumn == 'done') { enableConfetti(true) }
-		dragItem({ task: '', column: null, id: 0 })
-  }
+	const reorderColumn = (target) => {
+		const { column: targetColumn, id: targetId } = target;
+		const { task, column, id } = dragged;
 
-  const reorderColumn = (event, target) => {
-		const { column: targetColumn, id: targetId } = target 
-		const { task, column, id } = dragged
+		if (column !== targetColumn) return;
 
-		if (column != targetColumn) return;
+		const targetIndex = tasks[column].findIndex((elem) => elem.id === targetId);
+		const updatedColumn = tasks[column].filter(
+			({ id: taskId }) => taskId !== id,
+		);
 
-		const currentDraggedIndex = tasks[column].findIndex(elem => elem.id === id);
-		const targetIndex = tasks[column].findIndex(elem => elem.id === targetId);
-		const updatedColumn = tasks[column].filter(({ id: taskId }) => taskId !== id)
-
-		updatedColumn.splice(targetIndex, 0, { task, id })
+		updatedColumn.splice(targetIndex, 0, { task, id });
 
 		updateTasks({
 			...tasks,
-			[column]: updatedColumn
-		})
-		dragItem({ task: '', column: null, id: 0 })
-  }
+			[column]: updatedColumn,
+		});
+		dragItem({ task: "", column: null, id: 0 });
+	};
 
-  return (
-    <>
-		<section className='tasks'>
-			{ confettiEnabled ? <Confetti /> : null }
-			<Column column='todos' 
-				title='To do' 
-				tasks={tasks.todos} 
-				receiveItem={receiveItem} 
-				reorderColumn={reorderColumn} 
-				startDrag={startDrag}/> 
+	return (
+		<>
+			<section className="tasks">
+				{confettiEnabled ? <Confetti /> : null}
+				<Column
+					column="todos"
+					title="To do"
+					tasks={tasks.todos}
+					receiveItem={receiveItem}
+					reorderColumn={reorderColumn}
+					startDrag={startDrag}
+				/>
 
-			<Column column='doing' 
-				title='Doing' 
-				tasks={tasks.doing} 
-				receiveItem={receiveItem} 
-				reorderColumn={reorderColumn} 
-				startDrag={startDrag}/> 
+				<Column
+					column="doing"
+					title="Doing"
+					tasks={tasks.doing}
+					receiveItem={receiveItem}
+					reorderColumn={reorderColumn}
+					startDrag={startDrag}
+				/>
 
-			<Column column='done' 
-				title='Done' 
-				tasks={tasks.done} 
-				receiveItem={receiveItem} 
-				reorderColumn={reorderColumn} 
-				startDrag={startDrag}/> 
-		</section>
+				<Column
+					column="done"
+					title="Done"
+					tasks={tasks.done}
+					receiveItem={receiveItem}
+					reorderColumn={reorderColumn}
+					startDrag={startDrag}
+				/>
+			</section>
 
-		<section className='form'>
-			<form onSubmit={addTodo}>
-				<textarea name="todo" />
-				<button type="submit">Add</button>
-			</form>
-		</section>
-	</>
-  )
+			<section className="form">
+				<form onSubmit={addTodo}>
+					<textarea name="todo" />
+					<button type="submit">Add</button>
+				</form>
+			</section>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,31 +1,44 @@
-
 interface ColumnProps {
-  columnKey: string,
-  title: string,
-  tasks: Array<Task>,
-  receiveItem: (event: React.DragEvent, key: string) => void,
-  reorderColumn: (event: React.DragEvent, task: Task & { column: string }) => void,
-  startDrag: (task: Task & { column: string }) => void,
+	columnKey: string;
+	title: string;
+	tasks: Array<Task>;
+	receiveItem: (event: React.DragEvent, key: string) => void;
+	reorderColumn: (
+		event: React.DragEvent,
+		task: Task & { column: string },
+	) => void;
+	startDrag: (task: Task & { column: string }) => void;
 }
 
-export function Column({ column, title, tasks, receiveItem, reorderColumn, startDrag } : ColumnProps) {
+export function Column({
+	column,
+	title,
+	tasks,
+	receiveItem,
+	reorderColumn,
+	startDrag,
+}: ColumnProps) {
 	return (
-		<div className='item-column' 
-			onDragOver={(event) => event.preventDefault()} 
-			onDrop={(event) => receiveItem(event, column)}>
+		<div
+			className="item-column"
+			onDragOver={(event) => event.preventDefault()}
+			onDrop={(event) => receiveItem(column)}
+		>
 			<h1>{title}</h1>
-			<hr/>
-			<div className='items'>
-				{
-					tasks.map(({ id, task }) => (
-						<div className='item' draggable 
-							onDrop={(event) => reorderColumn(event, { id, task, column })} 
-							onDragStart={() => startDrag({ id, task, column })}
-							key={id}>{task}</div>
-					))
-				}
+			<hr />
+			<div className="items">
+				{tasks.map(({ id, task }) => (
+					<div
+						className="item"
+						draggable
+						onDrop={(event) => reorderColumn({ id, task, column })}
+						onDragStart={() => startDrag({ id, task, column })}
+						key={id}
+					>
+						{task}
+					</div>
+				))}
 			</div>
 		</div>
-	)
-
+	);
 }
